15947e2db4b6c397cb9b27e006e18704b96e173e
febriantimayori <febriantimayori@gmail.com>
2025-04-27 20:28:47 +0700

    chore: adjust user layout to integrate footer component

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,10 +1,12 @@
 import NavbarUser from '@/components/user/navbar-user'
+import FooterUser from '@/components/user/footer-user';
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <NavbarUser />
       <main className="pt-4 px-6">{children}</main>
+      <FooterUser />
     </>
   )
 }
