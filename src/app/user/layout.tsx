aa3fb90a144f37b9a276fbb8b8cd2e7e5f2af4e1
febriantimayori <febriantimayori@gmail.com>
2025-04-28 17:09:49 +0700

    import footer in homepage

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Metadata } from 'next'
+// import { Metadata } from 'next'
 import NavbarUser from '@/components/user/navbar-user'
 import FooterUser from '@/components/user/footer-user'
 
