926bb65ce6dabddaaea99cf009c9c7e3dc4445dc
febriantimayori <febriantimayori@gmail.com>
2025-05-15 11:48:59 +0700

    fix: separate client component and metadata config in user homepage

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
-// import { Metadata } from 'next'
 import NavbarUser from '@/components/user/navbar-user'
 import FooterUser from '@/components/user/footer-user'
-
-// export const metadata: Metadata = {
-//   title: 'Beranda User',
-//   description: 'Halaman utama bagi pengguna sport center',
-// }
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
