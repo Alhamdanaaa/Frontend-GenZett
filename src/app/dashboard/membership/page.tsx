3a06019bb0792a4b60fa76fdc0c192dfa72703a2
febriantimayori <febriantimayori@gmail.com>
2025-05-22 14:35:50 +0700

    perf: optimize MembershipListingPage loading

--- src/app/dashboard/membership/page.tsx
+++ src/app/dashboard/membership/page.tsx
@@ -3,13 +3,16 @@ import { buttonVariants } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import { DataTableSkeleton } from '@/components/ui/table/data-table-skeleton';
-import MembershipListingPage from '@/features/memberships/components/membership-listing';
 import { searchParamsCache, serialize } from '@/lib/searchparams';
 import { cn } from '@/lib/utils';
 import { IconPlus } from '@tabler/icons-react';
 import Link from 'next/link';
 import { SearchParams } from 'nuqs/server';
-import { Suspense } from 'react';
+import { lazy, Suspense } from 'react';
+
+const MembershipListingPage = lazy(() =>
+  import('@/features/memberships/components/membership-listing')
+);
 
 export const metadata = {
   title: 'Dashboard: Paket Membership'
@@ -21,10 +24,7 @@ type pageProps = {
 
 export default async function Page(props: pageProps) {
   const searchParams = await props.searchParams;
-  // Allow nested RSCs to access the search params (in a type-safe way)
   searchParamsCache.parse(searchParams);
-
-  // This key is used for invoke suspense if any of the search params changed (used for filters).
   const key = serialize({ ...searchParams });
 
   return (
